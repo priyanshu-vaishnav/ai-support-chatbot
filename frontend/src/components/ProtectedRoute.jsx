0a59a64b0da9964f959db748ae57188be263f8c7
priyanshu-vaishnav <priyanshuvaishnav0606@gmail.com>
2026-06-24 18:22:57 +0530

    nowpushingnewbranch

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,6 +3,9 @@ import { useAuth } from "../services/authContext"
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
-  if (loading) return <div style={{ padding: '40px' }}>Loading...</div>
-  return user ? children : <Navigate to="/" />
+  if (loading) return <div style={{ padding: '40px' }}>Loading Please wait...</div>
+  
+  
+  return user ? children : <Navigate to="/" /> 
+  
 }
